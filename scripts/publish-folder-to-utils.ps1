f6fd3c2559701c829b262842a234ca3c566f1dc4
tom.russell <tom.russell@integralanalytics.com>
2026-07-05 22:50:02 -0400

    fix: update default destination directory for publishing

--- scripts/publish-folder-to-utils.ps1
+++ scripts/publish-folder-to-utils.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param(
-    [string]$DestinationDir = 'D:\utils\MicrophoneManager',
+    [string]$DestinationDir = 'D:\utils',
     [string]$Project = 'MicrophoneManager.WinUI\MicrophoneManager.WinUI.csproj',
     [string]$PublishProfile = 'win-x64-singlefile'
 )
